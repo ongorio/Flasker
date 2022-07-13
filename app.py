e419788d750ec8b65844fbed99cad9538e5ccc5e
ongorio <ongorio5@gmail.com>
2022-07-12 20:28:28 -0500

    added basic app configration and post mdoel

--- app.py
+++ app.py
@@ -1,9 +1,61 @@
 from flask import Flask, render_template, Response
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+from datetime import datetime
+
+from utils.secrets import get_secrets
 
 app = Flask(__name__)
 
 
+# App Configuration
+secrets = get_secrets('env.json')
+
+app.config['SECRET_KEY'] = secrets['SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{secrets["DB"]["USERNAME"]}:{secrets["DB"]["PASSWORD"]}@{secrets["DB"]["HOST"]}/{secrets["DB"]["NAME"]}' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# DB Configuration
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.String(255), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    pub_date = db.Column(db.DateTime, default=datetime.today)
+
+    def __repr__(self):
+        return f'<Post> {self.title}'
+
+
 @app.route('/')
 def index():
-    return Response('<h1>Welcome to Flasker!</h1>')
+    posts = Post.query.all().order_by(Post.pub_date)
+
+    return render_template('index.html', posts=posts)
+
+
+# Posts Views
+
+
+
+
+# Error Handlers
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(403)
+def permission_denied(e):
+    return render_template('errors/403.html'), 403
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('errors/500.html'), 500
